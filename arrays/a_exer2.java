ef168f4c82157b65601f13f0b48c774abcfd91b2
dev-macedo <292881926+dev-macedo@users.noreply.github.com>
2026-09-01 13:06:58 -0300

    add basic array structure

---
+++ arrays/a_exer2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class a_exer2{
+    public static void main(String[] args){
+        int[] numeros = {12, 5, 23, 8, 19, 3};
+        
+       for(int i = 0; i<numeros.length; i++){
+        System.out.println(numeros[i]);
+       }
+        
+
+    }
+}
