3da6f58f41fc458fce1485b4c39bb5a663b28841
dev-macedo <292881926+dev-macedo@users.noreply.github.com>
2026-09-01 13:26:34 -0300

    exercise done

--- arrays/a_exer2.java
+++ arrays/a_exer2.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 public class a_exer2{
     public static void main(String[] args){
         int[] numeros = {12, 5, 23, 8, 19, 3};
+        int maior, menor;
+        float media = 0;
+        maior = menor = numeros[0];
         
        for(int i = 0; i<numeros.length; i++){
-        System.out.println(numeros[i]);
+        media += numeros[i];
+        //encontre o maior número
+        if(maior>numeros[i]){} 
+        else{
+            maior = numeros[i];
+        }
+        //encontre o menor número
+        if(menor<numeros[i]){} 
+        else{
+            menor = numeros[i];
+        }
        }
-        
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Media: " + media / numeros.length);
 
     }
 }
